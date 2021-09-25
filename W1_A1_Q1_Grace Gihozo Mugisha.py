e4b92c8b476cf8f529e02c8435488a7b840c3fb6
Grace Gihozo Mugisha <g.mugisha1@alustudent.com>
2021-09-24 18:49:29 -0700

    Initial commit

---
+++ W1_A1_Q1_Grace Gihozo Mugisha.py
@@ -0,0 +1,11 @@
+# Program that prints your initials in big letters,example, my name is Grace Gihozo Mugisha
+print("G G G G G G G G"      "    "       "G G G G G G G G"         "  "  "M                   M" )
+print("G"                    "    "       "G"                       "  "  "M   M            M  M" )
+print("G"                    "    "       "G"                       "  "  "M      M        M   M" )
+print("G"                    "    "       "G"                       "  "  "M       M     M     M" )
+print("G"                    "    "       "G"                       "  "  "M          M        M" )
+print("G"                    "    "       "G"                       "  "  "M                   M" )
+print("G        G G  G"      "    "       "G        G G  G"         "  "  "M                   M" )
+print("G             G"      "    "       "G             G"         "  "  "M                   M" )
+print("G             G"      "    "       "G             G"         "  "  "M                   M" )
+print("G G G G G G G G"      "    "       "G G G G G G G G"         "  "  "M                   M" )
